561663669004c0d6ae54a6ac263d3ef482bd15f5
Konstantin Sherbak <sh3rbak.kos@gmail.com>
2024-04-02 22:18:23 +0500

    Apples
    -add seed for rand();
    -add apples;
    -add collision with apples.

--- ApplesGame/GameMain.cpp
+++ ApplesGame/GameMain.cpp
@@ -7,9 +7,13 @@ const int SCREEN_HEIGHT = 720;
 const float INITIAL_SPEED = 100.f;
 const float PLAYER_SIZE = 20.f;
 const float ACCELERATION = 10.f;
+const int NUM_APPLES = 20;
+const float APPLE_SIZE = 20.f;
 
 int main()
 {
+    int seed = (int)time(nullptr);
+    srand(seed);
     sf::RenderWindow window(sf::VideoMode(SCREEN_WIGHT , SCREEN_HEIGHT), "APPLES GAME!");
     float playerX = SCREEN_WIGHT / 2.f;
     float playerY = SCREEN_HEIGHT / 2.f;
@@ -21,6 +25,26 @@ int main()
     playerShape.setFillColor(sf::Color::Yellow);
     playerShape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);
     playerShape.setPosition(playerX, playerY);
+
+    float appleX[NUM_APPLES];
+    float appleY[NUM_APPLES];
+    bool isAppleEaten[NUM_APPLES];
+    sf::CircleShape applesShape[NUM_APPLES];
+
+
+    for (int i = 0; i < NUM_APPLES; ++i)
+    {
+        isAppleEaten[i] = false;
+        appleX[i] = APPLE_SIZE + rand() / (float)RAND_MAX * (SCREEN_WIGHT - APPLE_SIZE);
+        appleY[i] = APPLE_SIZE + rand() / (float)RAND_MAX * (SCREEN_HEIGHT - APPLE_SIZE);
+
+        applesShape[i].setRadius(APPLE_SIZE / 2.f);
+        applesShape[i].setFillColor(sf::Color::Green);
+        applesShape[i].setOrigin(APPLE_SIZE / 2.f, APPLE_SIZE / 2.f);
+        applesShape[i].setPosition(appleX[i], appleY[i]);
+    }
+
+    int numEatenApples = 0;
 
     sf::Clock gameClock;
     float lastTime = gameClock.getElapsedTime().asSeconds();
@@ -81,10 +105,51 @@ int main()
             break;
         }
 
+        /*for (int i = 0; i < NUM_APPLES; ++i)
+        {
+            if (!isAppleEaten[i])
+            {
+                float deltaX = fabs(playerX - appleX[i]);
+                float deltaY = fabs(playerY - appleY[i]);
+                if (deltaX <= (APPLE_SIZE + PLAYER_SIZE) / 2.f &&
+                    deltaY <= (APPLE_SIZE + PLAYER_SIZE) / 2.f)
+                {
+                    isAppleEaten[i] = true;
+                    ++numEatenApples;
+                }
+            }
+        }*/
+        for (int i = 0; i < NUM_APPLES; ++i)
+        {
+            if (!isAppleEaten[i])
+            {
+                float deltaX = (playerX - appleX[i]) * (playerX - appleX[i]);
+                float deltaY = (playerY - appleY[i]) * (playerY - appleY[i]);
+                float distance = deltaX + deltaY;
+                if (distance <= (APPLE_SIZE + PLAYER_SIZE) * (APPLE_SIZE + PLAYER_SIZE) / 4)
+                {
+                    isAppleEaten[i] = true;
+                    ++numEatenApples;
+                }
+            }
+        }
+
+        if (numEatenApples == NUM_APPLES)
+        {
+            window.close();
+            break;
+        }
 
         window.clear();
         playerShape.setPosition(playerX, playerY);
         window.draw(playerShape);
+        for (int i = 0; i < NUM_APPLES; ++i)
+        {
+            if(!isAppleEaten[i])
+            {
+                window.draw(applesShape[i]);
+            }
+        }
         window.display();
     }
 
